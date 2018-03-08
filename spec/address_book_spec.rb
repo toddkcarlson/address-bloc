89aa75dcb699f8bdde3c911e1ec36024f8d53378
Todd Carlson <tcarlson@carlsonsw.com>
2018-03-07 21:40:44 -0500

    Checkpoint 4: Completed

---
+++ spec/address_book_spec.rb
@@ -0,0 +1,21 @@
+ require_relative '../models/address_book'
+
+ RSpec.describe AddressBook do
+   describe "attributes" do
+     it "responds to entries" do
+       book = AddressBook.new
+       expect(book).to respond_to(:entries)
+     end
+ 
+     it "initializes entries as an array" do
+       book = AddressBook.new
+       expect(book.entries).to be_an(Array)
+     end
+ 
+     it "initializes entries as empty" do
+       book = AddressBook.new
+       expect(book.entries.size).to eq(0)
+     end
+   end
+
+ end
