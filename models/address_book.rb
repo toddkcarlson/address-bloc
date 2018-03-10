08cf44dd536cb6f58f33d1180bf7a47e37b6825c
Todd Carlson <tcarlson@carlsonsw.com>
2018-03-10 14:54:01 -0500

    checkpoint 5 csv

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,5 +1,6 @@
 # #8
 require_relative 'entry'
+require "csv"
 
 
  class AddressBook
@@ -22,4 +23,15 @@ require_relative 'entry'
      # #11
      entries.insert(index, Entry.new(name, phone_number, email))
    end
+
+   def import_from_csv(file_name)
+     # Implementation goes here
+     csv_text = File.read(file_name)
+     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+   # #8
+     csv.each do |row|
+       row_hash = row.to_hash
+       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+     end     
+   end  
  end
