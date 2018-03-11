6e5af7995051fcaa05b26fe07115b5a534aa88c7
Todd Carlson <tcarlson@carlsonsw.com>
2018-03-11 15:11:45 -0400

    assignment 7 search

--- models/address_book.rb
+++ models/address_book.rb
@@ -33,5 +33,40 @@ require "csv"
        row_hash = row.to_hash
        add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
      end     
-   end  
+   end
+   # Search AddressBook for a specific entry by name
+   def binary_search(name)
+     lower = 0
+     upper = entries.length - 1
+ 
+     # #2
+     while lower <= upper
+       # #3
+       mid = (lower + upper) / 2
+       mid_name = entries[mid].name
+ 
+       # #4
+       if name == mid_name
+         return entries[mid]
+       elsif name < mid_name
+         upper = mid - 1
+       elsif name > mid_name
+         lower = mid + 1
+       end
+     end
+ 
+     # #5
+     return nil   
+   end
+
+   def iterative_search(name)
+
+     entries.each do |entry|
+      if entry.name == name
+        return entry
+      end
+     end
+      return nil
+   end
+
  end
